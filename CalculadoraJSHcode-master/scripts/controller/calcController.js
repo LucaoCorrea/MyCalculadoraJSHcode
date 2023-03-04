2bae391140dae3a6b4b1ff2ff9e1b949cf9d0a03
LucaoCorrea <lucascorrea1505@gmail.com>
2023-03-03 23:38:27 -0300

    Até a aula 10

--- CalculadoraJSHcode-master/scripts/controller/calcController.js
+++ CalculadoraJSHcode-master/scripts/controller/calcController.js
@@ -1,24 +1,67 @@
 class CalcController {
   constructor() {
-    this._displayCalc = '0';
-    this._dataAtual;
+    this._locale = 'pt-BR';
+    this._displayCalcEl = document.querySelector('#display');
+    this._dateEl = document.querySelector('#data');
+    this._timeEl = document.querySelector('#hora'); 
+
+    this._currentDate;
+    this.initialize();
   }
-  
-//displayCalc
+
+  initialize() {
+
+    this.setDisplayDateTime();
+
+    setInterval(() => {
+      this.setDisplayDateTime();
+    }, 1000);
+
+  }
+
+  setDisplayDateTime(){
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit",
+      month: "long",
+      year: "numeric"
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+
+  //displayTime
+  get displayTime(){
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value){
+    return this._timeEl.innerHTML = value;
+  }
+
+  //displayDate
+  get displayDate(){
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value){
+    return this._dateEl.innerHTML = value;
+  }
+
+
+  //displayCalc
   get displayCalc() {
-    return this._displayCalc;
+    return this._displayCalcEl.innerHTML;
   }
 
-  set displayCalc(valor) {
-    this.displayCalc = valor;
+  set displayCalc(value) {
+    this._displayCalcEl.innerHTML = value;
   }
 
-//dataAtual
-  get dataAtual() {
-    return this._dataAtual;
+  //currentDate
+  get currentDate() {
+    return new Date();
   }
 
-  set dataAtual(valor) {
-    this._dataAtual = valor;
+  set currentDate(value) {
+    this._currentDate = value;
   }
 }
